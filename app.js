d2660eb9a50c20aa9791829e066987f7e51680c8
Medi G Fulgence <lycorisblue99@gmail.com>
2025-01-27 02:26:00 +0000

    fix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');  // N'oubliez pas d'installer cors
+const helmet = require('helmet');
 const oneSignalClient = require('./utils/OneSignalClient');
 
 // Import des controllers
