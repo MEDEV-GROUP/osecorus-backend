1a0182cbbac0d26220df2c3c2db3ad8512a6ac32
Medi G Fulgence <lycorisblue99@gmail.com>
2025-07-05 21:26:49 +0000

    feat: ajouter des attributs pour la récupération des établissements de santé

--- routes/etablissement-sante/getNearbyEtablissementsSante.js
+++ routes/etablissement-sante/getNearbyEtablissementsSante.js
@@ -64,6 +64,7 @@ router.post('/', async (req, res) => {
                     [Op.not]: null
                 }
             },
+            attributes: ['id', 'nom_etablissement', 'categorie', 'ville_commune', 'quartier', 'latitude', 'longitude'],
             order: [['nom_etablissement', 'ASC']]
         });
 
